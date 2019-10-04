f7b923d37cfb2682c6d5c58cc6f6514f315ff631
Luuk Siewers <luuk@siewers.org>
2019-10-04 13:00:42 +0200

    game online

---
+++ VR Basketball/Assets/Basketball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Basketball : MonoBehaviour
+{
+    [SerializeField] GameObject missesText;
+    [SerializeField] GameObject goalsText;
+    private int misses = 0;
+    private int goals = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.name == "End detector") {
+            misses++;
+            missesText.GetComponent<TextMeshProUGUI>().SetText(misses.ToString() + " Misses");
+        } 
+        if (other.gameObject.name == "Score point") {
+            goals++;
+            goalsText.GetComponent<TextMeshProUGUI>().SetText(goals.ToString() + " Goals");
+        }
+        if (other.gameObject.name == "Score point" || other.gameObject.name == "End detector") {
+            int randomX = Random.Range(-6, 6);
+            transform.position = new Vector3(randomX, 5, 5);
+            Debug.Log(randomX);
+        }
+    }
+}
---
+++ VR Basketball/Assets/Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    private int rotationSpeed = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15));
+
+        if(Input.GetMouseButtonDown(0)) {
+            transform.Rotate(transform.rotation.x - rotationSpeed, transform.rotation.y - rotationSpeed, 5);
+        } else if (Input.GetMouseButtonDown(1)) {
+            transform.Rotate(transform.rotation.x + rotationSpeed, transform.rotation.y + rotationSpeed, 5);
+        }
+    }
+}
