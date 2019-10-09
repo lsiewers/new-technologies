684ae15ef37185284ad94fcec8cd1936b65d04a2
Marijke ter Pelle <m.c.terpelle@gmail.com>
2019-10-09 20:18:00 +0200

    Push git ding

---
+++ VR Basketball V2.0/Assets/AudioTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioTest : MonoBehaviour
+{
+    public AudioSource soundSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //IEnumerator JustWait()
+    //{
+        //print(Time.time);
+        //yield return new WaitForSeconds(0.5f);
+        //print(Time.time);
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.tag == "Platform")
+        //{
+            //soundSource.Play();
+            //StartCoroutine(JustWait());
+        //Destroy(GetComponent<Rigidbody>());        
+            //Destroy(collision.gameObject);
+        //}
+    }
+}
---
+++ VR Basketball V2.0/Assets/cloudboi.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudboi : MonoBehaviour
+{
+
+    [SerializeField] GameObject cam;
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(0.5f, 2.5f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(cam.transform.position, Vector3.up, Time.deltaTime * speed);
+    }
+}
